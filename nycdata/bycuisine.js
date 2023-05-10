facd15552527b098478e8d2272e6b3fc3c47a6ec
enadiyaa <112989777+enadiyaa@users.noreply.github.com>
2023-05-10 17:32:59 -0400

    ff

--- nycdata/bycuisine.js
+++ nycdata/bycuisine.js
@@ -1,65 +1,89 @@
+
 window.onload = async function () {
-    const response = await fetch("./americanfood.json");
-    const data = await response.json();
-    let countCritical = 0;
-    let countNotCritical = 0;
-    let countNotApplicable = 0;
-    data.forEach(item => {
-        if (item.critical_flag === "Critical") {
-            countCritical += 1;
-        }
-        if (item.critical_flag === "Not Critical") {
-            countNotCritical += 1;
-        }
-        if (item.critical_flag === "Not Applicable") {
-            countNotApplicable += 1;
-        }
-    })
-
-    const ctx = document.getElementById('pieChart');
-
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ["Critical", "Not Critical", "Not Applicable"],
-            datasets: [{
-                label: '# of Votes',
-                data: [countCritical, countNotCritical, countNotApplicable],
-                borderWidth: 1,
-                backgroundColor: [
-                    'green',
-                    '#FAC60F',
-                    'plum'],
-            }]
+    const dataframes = [
+        {
+            filename: "./americanfood.json",
+            canvasId: "pieChart",
+            name: "american food"
         },
-        options: {
+        {
+            filename: "./mecianfood.json",
+            canvasId: "pieChart1",
+            name: "mexican food"
+        },
+        {
+            filename: "./chinesefood.json",
+            canvasId: "pieChart2"
+        },
+        {
+            filename: "./italianfood.json",
+            canvasId: "pieChart3"
+        }
+    ];
+    
+    dataframes.forEach(async element => {
+        const response = await fetch(element.filename);
+        const data = await response.json();
+        let countCritical = 0;
+        let countNotCritical = 0;
+        let countNotApplicable = 0;
+        data.forEach(item => {
+            if (item.critical_flag === "Critical") {
+                countCritical += 1;
+            }
+            if (item.critical_flag === "Not Critical") {
+                countNotCritical += 1;
+            }
+            if (item.critical_flag === "Not Applicable") {
+                countNotApplicable += 1;
+            }
+        })
 
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Cuisine: American',
-                    color:"white",
-                    font: {
-                        size: 30,
-                        family: "'Owners1'",
-                    }
-                },
-                legend: {
-                    labels: {
+        const ctx = document.getElementById(element.canvasId);
+
+        new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ["Critical", "Not Critical", "Not Applicable"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [countCritical, countNotCritical, countNotApplicable],
+                    borderWidth: 1,
+                    backgroundColor: [
+                        'green',
+                        '#FAC60F',
+                        'plum'],
+                }]
+            },
+            options: {
+    
+                plugins: {
+                    title: {
+                        display: true,
+                        text: element.name,
                         color:"white",
                         font: {
-                            size: 14,
+                            size: 30,
                             family: "'Owners1'",
                         }
+                    },
+                    legend: {
+                        labels: {
+                            color:"white",
+                            font: {
+                                size: 14,
+                                family: "'Owners1'",
+                            }
+                        }
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        display:false
                     }
                 }
-            },
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    display:false
-                }
             }
-        }
-    });
+        });
+    })
 }
